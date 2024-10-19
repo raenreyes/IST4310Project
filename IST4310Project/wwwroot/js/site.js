c48e59cfb74d45466529f2d596e5177b9011692a
raenreyes <raen.reyes@gmail.com>
2024-10-18 18:04:46 -0700

    commit

--- IST4310Project/wwwroot/js/site.js
+++ IST4310Project/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $('.holder').click(function () {
+        alert("Logging Out...");
+        window.location.href = "https://localhost:7093/"
+    })
+});
